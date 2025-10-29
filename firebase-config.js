6c1162296b1a98bc579e61628ae2299b51996a76
thisisloserbumblebee-star <thisisloserbumblebee@gmail.com>
2025-10-30 03:07:47 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,27 @@
 <script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-auth.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-firestore.js";
-  import { getStorage } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-storage.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-storage.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyDfldfkopj9k_y6s44rsCQRKfXCV6uSCyS",
-    authDomain: "pehchaan-setu.firebaseapp.com",
-    projectId: "pehchaan-setu",
-    storageBucket: "pehchaan-setu.appspot.com",
-    messagingSenderId: "868115882621",
-    appId: "1:868115882621:web:461eee8266f1bd22818714",
-    measurementId: "G-53DQYFKN7R"
-  };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfhdfkopi9k_y6s44rsCQRfKfXCGUsCYs",
+  authDomain: "pehchaan-setu.firebaseapp.com",
+  projectId: "pehchaan-setu",
+  storageBucket: "pehchaan-setu.appspot.com",
+  messagingSenderId: "868115882621",
+  appId: "1:868115882621:web:4617eee0266fb1d22818714",
+  measurementId: "G-SC0GNZHMY5"
+};
 
-  const app = initializeApp(firebaseConfig);
-  window.__firebase = {
-    auth: getAuth(app),
-    db: getFirestore(app),
-    storage: getStorage(app)
-  };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+// Make available globally
+window.__firebase = { app, auth, db, storage };
 </script>
